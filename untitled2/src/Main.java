14c8af6a8603cbed6fa928524155183fc6499c0f
OlegD <oleg.n.dobkin@gmail.com>
2024-10-12 23:32:52 +0300

    ura

---
+++ untitled2/src/Main.java
@@ -0,0 +1,27 @@
+
+
+import java.util.Arrays;
+
+
+
+
+
+public class Main {
+    public static String[] array = new String[]{"as","sd ", "fg","gh","ff","yt"};
+
+
+    public static void main(String[] args) {
+        String[] x1 = Arrays.copyOfRange(array, 3, 6);
+        System.out.println(Arrays.toString(x1));
+
+
+
+
+        }
+
+}
+
+// Перебираем все значения массива
+
+
+
